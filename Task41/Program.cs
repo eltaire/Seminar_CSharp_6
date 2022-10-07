b59f9cc9106cf868b09d20161bbbfa2e1fd04ae2
Elta <eltaire@gmail.com>
2022-10-07 11:36:05 +0300

    HW

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число элементов Вашего массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] Massiv = new int[n];
+void FillMassiv(int n)
+     {
+        int i = 0;
+       while (i < n)
+        {
+           Console.WriteLine("Введите элемент массива");
+           Massiv[i] = Convert.ToInt32(Console.ReadLine());
+           i++;
+        }
+   }
+int Comparison(int[] Massiv)
+    {   
+        int count = 0;
+        for (int i = 0; i < Massiv.Length; i++)
+        {
+            if(Massiv[i] > 0 ) count += 1; 
+        }
+    return count;
+}
+
+FillMassiv(n);
+
+Console.WriteLine($"Количество чисел в Вашем массиве, которые больше 0, равно {Comparison(Massiv)}. ");
---
+++ Task43_dm/Program.cs
@@ -0,0 +1,43 @@
+﻿double[,] elements = new double[2, 2];
+double[] xPoint = new double[2];
+
+void InputElements()
+{
+  for (int i = 0; i < elements.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b) \n");
+    for (int j = 0; j < elements.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"введите коэффициент k: ");
+      else Console.Write($"введите коэффициент b: ");
+      elements[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Calculation(double[,] el)
+{
+  xPoint[0] = (el[1,1] - el[0,1]) / (el[0,0] - el[1,0]);
+  xPoint[1] = xPoint[0] * el[0,0] + el[0,1];
+  return xPoint;
+}
+
+void OutputResponse(double[,] el)
+{
+  if (el[0,0] == el[1,0] && el[0,1] == el[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (el[0,0] == el[1,0] && el[0,1] != el[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    Calculation(el);
+    Console.Write($"\nТочка пересечения прямых: ({xPoint[0]}, {xPoint[1]})");
+  }
+}
+
+InputElements();
+OutputResponse(elements);
